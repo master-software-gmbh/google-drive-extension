73b11fca29f6e07412692531e062410ab3c20656
Marian Heinsen <mail@marianheinsen.com>
2022-08-17 17:54:49 +0200

    first commit

---
+++ drive.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", setupListener);
+
+function setupListener() {
+  const observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+      mutation.addedNodes.forEach((node) => {
+        if (node.nodeName == 'DIV' && node.classList.contains('a-X-k')) {
+          document.querySelector("body > div.a-X-k > div.a-X-k-Gb > button.h-De-Vb.h-De-Y.a-X-d").click();
+          node.parentElement.removeChild(node);
+        }
+      })
+    });
+  });
+
+  observer.observe(document.body, {
+    childList: true,
+  });
+}
