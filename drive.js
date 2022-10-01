ce34891daa19ef8e815884e3985690f9a7987769
Marian Heinsen <mail@marianheinsen.com>
2022-10-01 20:02:05 +0200

    prevent flashing

--- drive.js
+++ drive.js
@@ -6,6 +6,7 @@ function setupListener() {
       mutation.addedNodes.forEach((node) => {
         if (node.nodeName == 'DIV' && node.classList.contains('a-X-k')) {
           document.querySelector("body > div.a-X-k > div.a-X-k-Gb > button.h-De-Vb.h-De-Y.a-X-d").click();
+          node.parentElement.removeChild(node.previousSibling);
           node.parentElement.removeChild(node);
         }
       })
