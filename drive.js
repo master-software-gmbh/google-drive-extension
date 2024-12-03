f59c266649c263d87300a344bfefb8e08dacbd6f
Marian Eickmeyer <mail@marianheinsen.com>
2024-12-03 17:23:44 +0100

    feat: auto-confirm popups when creating new files in shared folders

--- drive.js
+++ drive.js
@@ -8,8 +8,12 @@ function setupListener() {
           document.querySelector("body > div.a-X-k > div.a-X-k-Gb > button.h-De-Vb.h-De-Y.a-X-d").click();
           node.parentElement.removeChild(node.previousSibling);
           node.parentElement.removeChild(node);
+        } else if (node.nodeName == 'DIV' && node.classList.value == 'lb-k g-ge') {
+          document.querySelector("body > div.lb-k.g-ge > div.lb-k-Gb > button.h-De-Vb.h-De-Y").click();
+          node.parentElement.removeChild(node.previousSibling);
+          node.parentElement.removeChild(node);
         }
-      })
+      });
     });
   });
 
